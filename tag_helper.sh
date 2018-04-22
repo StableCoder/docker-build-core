611b00fbcc40e9d7f58c94bb8e24602cce21bf5d
George Cave <gcave@stablecoder.ca>
2018-04-22 15:38:33 -0400

    Fixed tag helper script
    Now properly expands the environment variables in the sed statement.

--- tag_helper.sh
+++ tag_helper.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+TAG=${RELEASE}
+
 if [ "${RELEASE}" == "" ]; then
-    RELEASE=latest
+    TAG=latest
 fi
 
-sed -i '1s/.*/FROM ${OS}:${RELEASE}/' ${OS}/${OS}${RELEASE}/gcc.dockerfile
-sed -i '1s/.*/FROM ${OS}:${RELEASE}/' ${OS}/${OS}${RELEASE}/clang.dockerfile
+sed -i "1s/.*/FROM $OS:$TAG/" ${OS}/${OS}${RELEASE}/gcc.dockerfile
+sed -i "1s/.*/FROM $OS:$TAG/" ${OS}/${OS}${RELEASE}/clang.dockerfile
