43370ab816bda7fe626b7f05062d3b3fe65ddf39
George Cave <gcave@stablecoder.ca>
2018-06-25 20:07:00 -0400

    Moved windows scripts around, improved
    Some refinements to the scripts has occurred as well.

--- windows/msvc2017/lib-scripts/msvc2017.ps1
+++ windows/msvc2017/lib-scripts/msvc2017.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install popular programs
-choco install -y 7zip cmake python git ninja svn strawberryperl
+choco install -y 7zip cmake python git ninja svn
 
 # Add new tools to machine path
 [Environment]::SetEnvironmentVariable( "Path", [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";C:\Program Files\7-Zip;C:\Program Files\CMake\bin;C:\Program Files\Git\bin;C:\Python36;C:\Python36\Scripts", [System.EnvironmentVariableTarget]::Machine )
--- windows/msvc2017/lib-scripts/qt4.ps1
+++ windows/msvc2017/lib-scripts/qt4.ps1
@@ -1,3 +1,5 @@
+choco install -y activeperl
+
 .\setup_msvc2017_x64.ps1
 & { iwr https://download.qt.io/archive/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz -OutFile qt-src.tar.gz }
 7z x qt-src.tar.gz
@@ -6,8 +8,11 @@ Remove-Item -path qt-src.tar.gz -Recurse -ErrorAction SilentlyContinue
 Remove-Item -path qt-src.tar -Recurse -ErrorAction SilentlyContinue
 
 cd qt-everywhere-opensource-src-4.8.7
-git apply .\qt4-2017.patch
+git apply --ignore-space-change --ignore-whitespace ..\qt4-2017.patch
 configure -release -make nmake -platform win32-msvc2017 -prefix c:\Qt-4.8.7 -opensource -confirm-license -opengl desktop -nomake examples -nomake tests -no-webkit
 nmake
 nmake install
+cd ..
+Remove-Item -path qt-everywhere-opensource-src-4.8.7 -Recurse -ErrorAction SilentlyContinue
 
+[Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\Qt-4.8.7\bin", [System.EnvironmentVariableTarget]::Machine )
