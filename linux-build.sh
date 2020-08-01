99f11653538817aa7e5e8844f8c0f6ea1c81e163
George Cave <gcave@stablecoder.ca>
2020-08-01 15:23:11 -0400

    Removed builder commands from scripts

--- linux-build.sh
+++ linux-build.sh
@@ -4,5 +4,5 @@ cd $OS
 cp ../entrypoint.sh .
 
 for FILE in *.Dockerfile; do
-    docker buildx build --builder job_${CI_JOB_ID} --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t test:${FILE%.*} -f ${FILE} .
+    docker buildx build  --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t test:${FILE%.*} -f ${FILE} .
 done
--- linux-upload.sh
+++ linux-upload.sh
@@ -4,6 +4,6 @@ cd $OS
 cp ../entrypoint.sh .
 
 for FILE in *.Dockerfile; do
-    docker buildx build --builder job_${CI_JOB_ID} --push --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} . || echo
-    docker buildx build --builder job_${CI_JOB_ID} --push ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} .
+    docker buildx build  --push --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} . || echo
+    docker buildx build  --push ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} .
 done
