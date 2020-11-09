4364ea68dfd7285dcc0ffde41f6da70a259bddd7
George Cave <gcave@stablecoder.ca>
2020-11-09 03:05:07 -0500

    Updated Linux scripts
    They now exit on error, and simplified the push script now that the
    autohorization delay issue is fixed.

--- linux-build.sh
+++ linux-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
+set -e
 
 cd $OS
 cp ../entrypoint.sh .
 
 for FILE in *.Dockerfile; do
-    docker buildx build  --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t test:${FILE%.*} -f ${FILE} .
+    docker buildx build --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t test:${FILE%.*} -f ${FILE} .
 done
--- linux-upload.sh
+++ linux-upload.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
+set -e
 
 cd $OS
 cp ../entrypoint.sh .
 
 for FILE in *.Dockerfile; do
-    docker buildx build  --push --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} . || echo
-    docker buildx build  --push ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} .
+    docker buildx build --push --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} .
 done
