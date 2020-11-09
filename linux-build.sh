614fd60729bcced4e8fa00fa731104907df36f01
George Cave <gcave@stablecoder.ca>
2020-11-09 14:54:26 -0500

    Merged the two linux scripts to one
    The ability to push can be passed in either by the EXTRA_BUILD_OPTIONS
    environment variable or with the --push script CL option.
    
    The tag is now passed in as a CL option.

--- linux-build.sh
+++ linux-build.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/bash
 set -e
 
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+    --tag)
+        TAG="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    --push)
+        PUSH="--push"
+        shift # past argument
+        ;;
+    esac
+done
+
 cd $OS
 cp ../entrypoint.sh .
 
 for FILE in *.Dockerfile; do
-    docker buildx build --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t test:${FILE%.*} -f ${FILE} .
+    docker buildx build --pull ${PUSH} ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${TAG}:${FILE%.*} -f ${FILE} .
 done
--- linux-upload.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-cd $OS
-cp ../entrypoint.sh .
-
-for FILE in *.Dockerfile; do
-    docker buildx build --push --pull ${EXTRA_BUILD_OPTIONS} --platform ${PLATFORMS} -t ${DOCKER_REGISTRY}${CORE_NAME}:${FILE%.*} -f ${FILE} .
-done
