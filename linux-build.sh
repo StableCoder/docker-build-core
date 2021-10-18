336a9e511c213587db6a3d577ed73fe9f490b745
George Cave <gcave@stablecoder.ca>
2021-10-18 10:54:39 -0400

    Added 'localhost' prefix to working copy images
    When pushing to the remote, podman/buildah will use remote-based images
    first. To keep using local variants, need to prefix with 'localhost'.

--- linux-build.sh
+++ linux-build.sh
@@ -29,27 +29,27 @@ for FILE in $OS/*.Dockerfile; do
     FILE="$(basename -- $FILE)"
 
     # Remove any previous manifest
-    echo "> Attempting to remove any previous manifest for $TAG:${FILE%.*}"
-    if podman manifest rm $TAG:${FILE%.*}; then
-        echo "> Removed old manifest for $TAG:${FILE%.*}"
+    echo "> Attempting to remove any previous manifest for localhost/$TAG:${FILE%.*}"
+    if podman manifest rm localhost/$TAG:${FILE%.*}; then
+        echo "> Removed old manifest for localhost/$TAG:${FILE%.*}"
     fi
 
     # Go through each platform and build the image for it, adding to the common manifest tag
     IFS=','
     for PLATFORM in $(cat $OS/${FILE%.*}.cfg); do
-        echo "> Running: podman build --pull --layers $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest $TAG:${FILE%.*} --tag $TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) ."
-        podman build --pull --layers $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest $TAG:${FILE%.*} --tag $TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
+        echo "> Running: podman build --pull --layers $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) ."
+        podman build --pull --layers $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
     done
 
     # If selected, login and push the manifest tag and associated images
     if [ ! -z $PUSH ]; then
         podman login -u $DOCKER_USER -p $DOCKER_PASSWORD docker.io
         # Using the v2s2 format to 
-        echo "> Running: podman manifest push --all --format v2s2 ${TAG}:${FILE%.*} docker://$TAG:${FILE%.*}"
-        podman manifest push --all -f v2s2 $TAG:${FILE%.*} docker://$TAG:${FILE%.*}
+        echo "> Running: podman manifest push --all --format v2s2 localhost/${TAG}:${FILE%.*} docker://$TAG:${FILE%.*}"
+        podman manifest push --all -f v2s2 localhost/$TAG:${FILE%.*} docker://$TAG:${FILE%.*}
     fi
 
     # Cleanup any manifest
-    echo "> Remove image $TAG:${FILE%.*}"
-    podman manifest rm $TAG:${FILE%.*}
+    echo "> Remove image localhost/$TAG:${FILE%.*}"
+    podman manifest rm localhost/$TAG:${FILE%.*}
 done
