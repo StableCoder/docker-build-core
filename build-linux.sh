373502576d5ece8f44730feb2e94a0d558acd600
George Cave <gcave@stablecoder.ca>
2024-09-11 09:10:08 -0400

    Modify directory handling for Linux images
    Change the selection of a directory from the `OS` variable to always be
    given to the script.
    
    As well, rather than the script keeping the working directory as the
    root, it will now change into the given directory, if given.

--- build-linux.sh
+++ build-linux.sh
@@ -6,6 +6,7 @@ GREEN='\033[0;32m'
 NO_COLOUR='\033[0m'
 
 NO_CACHE=
+IMAGE_DIR=
 TAG=
 PUSH=
 RM=
@@ -19,8 +20,8 @@ while [[ $# -gt 0 ]]; do
         NO_CACHE="--no-cache"
         shift
         ;;
-    -o | --os)
-        OS="$2"
+    -d | --image-dir)
+        IMAGE_DIR="$2"
         shift
         shift
         ;;
@@ -47,8 +48,8 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [[ -z "$OS" ]]; then
-    printf "${RED}> ERROR${NO_COLOUR} No OS specified\n" 1>&2
+if [[ ! -z "$IMAGE_DIR" ]] && [[ ! -d "$IMAGE_DIR" ]]; then
+    printf "${RED}> ERROR${NO_COLOUR} Directory '$IMAGE_DIR' does not exist\n"
     ERROR=1
 fi
 if [[ -z "$TAG" ]]; then
@@ -59,7 +60,11 @@ if [[ ! -z $ERROR ]]; then
     exit 1
 fi
 
-for FILE in $OS/*.Dockerfile; do
+if [[ ! -z "$IMAGE_DIR" ]]; then
+    cd $IMAGE_DIR
+fi
+
+for FILE in *.Dockerfile; do
     # Get platforms for the image
     PLATFORMS=$(grep -e '^# PLATFORMS: ' $FILE | head -n 1)
     PLATFORMS=${PLATFORMS:13}
@@ -80,14 +85,14 @@ for FILE in $OS/*.Dockerfile; do
     # Go through each platform and build the image for it, adding to the common manifest tag
     for PLATFORM in $PLATFORMS; do
         if [[ "${FILE%.*}" == *"-"* ]]; then
-            printf "${GREEN}>${NO_COLOUR} Running: podman build --pull $LAYERS $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .\n"
-            BUILDAH_LAYERS=false podman build --pull $LAYERS $NO_CACHE --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
+            printf "${GREEN}>${NO_COLOUR} Running: podman build --pull $LAYERS $NO_CACHE --platform $PLATFORM --file $FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .\n"
+            BUILDAH_LAYERS=false podman build --pull $LAYERS $NO_CACHE --platform $PLATFORM --file $FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
         else
             if podman image exists localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM); then
                 podman rmi localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM)
             fi
-            printf "${GREEN}>${NO_COLOUR} Running: podman build --pull $LAYERS --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .\n"
-            BUILDAH_LAYERS=false podman build --pull $LAYERS --platform $PLATFORM --file $OS/$FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
+            printf "${GREEN}>${NO_COLOUR} Running: podman build --pull $LAYERS --platform $PLATFORM --file $FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .\n"
+            BUILDAH_LAYERS=false podman build --pull $LAYERS --platform $PLATFORM --file $FILE --manifest localhost/$TAG:${FILE%.*} --tag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM) .
         fi
     done
 
@@ -103,12 +108,10 @@ for FILE in $OS/*.Dockerfile; do
     podman manifest rm localhost/$TAG:${FILE%.*}
 done
 
-
 if [ ! -z $RM ]; then
-    for FILE in $OS/*.Dockerfile; do
+    for FILE in *.Dockerfile; do
         FILE="$(basename -- $FILE)"
-        IFS=','
-        for PLATFORM in $(cat $OS/${FILE%.*}.cfg); do
+        for PLATFORM in $(head -n1 ${FILE%.*}.Dockerfile | cut -d ' ' -f3- | tr ' ' '\n'); do
            if podman untag localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM); then
                 echo "> Untagged image: localhost/$TAG:${FILE%.*}-$(cut -d '/' -f2 <<<$PLATFORM)"
            fi
